6c2e503e1a3eab178a51e4b162c63186557f908b
codexyan <mdcodeid@gmail.com>
2023-10-19 08:41:55 +0700

    update codes again guys

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -12,9 +12,11 @@ import Images from '../components/homepage/hero/Images.vue';
                         <h1 class="text-xl font-bold text-gray-300 md:text-4xl">
                             👨‍💻 Learn by Writing Anything
                         </h1>
-                        <p class="text-sm font-light text-gray-400">All my insightful reflections on programming and
+                        <p class="text-xs font-light text-gray-400 md:text-sm">All my insightful reflections on programming
+                            and
                             personal growth.</p>
                     </div>
+
                     <div class="input-search">
                         <label class="relative block">
                             <span class="sr-only">Search</span>
@@ -22,24 +24,26 @@ import Images from '../components/homepage/hero/Images.vue';
                                 <svg class="w-5 h-5 fill-white" viewBox="0 0 20 20"><!-- ... --></svg>
                             </span>
                             <input
-                                class="block w-2/3 py-2 pr-3 bg-gray-800 border rounded-lg shadow-sm placeholder:italic placeholder:text-slate-400 border-slate-300 pl-9 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                                class="block w-full py-2 pr-3 bg-gray-800 border rounded-lg shadow-sm md:w-2/3 placeholder:italic placeholder:text-slate-400 border-slate-300 pl-9 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
                                 placeholder="Search for anything..." type="text" name="search" />
                         </label>
                     </div>
-                    <div class="flex flex-row items-center gap-3 filters">
-                        <h1 class="text-gray-300">Filters :</h1>
-                        <a href="#" class="px-3 py-1 text-gray-400 bg-gray-800 rounded-sm">General</a>
-                        <a href="#" class="px-3 py-1 text-gray-400 bg-gray-800 rounded-sm">Programming</a>
-                        <a href="#" class="px-3 py-1 text-gray-400 bg-gray-800 rounded-sm">Insight</a>
+
+                    <div class="flex flex-row flex-wrap items-center gap-3 filters">
+                        <h1 class="text-gray-300 text-md md:text-base">Filters :</h1>
+                        <a href="#" class="px-3 py-1 text-xs text-gray-400 bg-gray-800 rounded-sm md:text-base">General</a>
+                        <a href="#"
+                            class="px-3 py-1 text-xs text-gray-400 bg-gray-800 rounded-sm md:text-base">Programming</a>
                     </div>
 
                     <div class="flex flex-col gap-6 card-blog md:mt-10">
                         <div class="card">
-                            <div class="flex flex-row-reverse items-start gap-6 md:flex-row row">
-                                <img class="object-cover h-full rounded-md w-28 md:h-36 md:w-56"
-                                    src="@/assets/img/images/img-card-artdir.png" alt="card.png" />
+                            <div class="flex flex-row items-start gap-6 md:flex-row row">
+                                <div class="bg-red-300 images-card">
+                                    <img class="h-full" src="@/assets/img/images/img-card-artdir.png" alt="card.png" />
+                                </div>
                                 <div class="flex flex-col gap-4 content-card">
-                                    <a href="#" class="text-lg font-bold text-gray-200 md:text-xl">
+                                    <a href="#" class="text-sm font-bold text-gray-200 md:text-xl">
                                         Nekat Jadi PM Director
                                     </a>
                                     <p
@@ -52,11 +56,12 @@ import Images from '../components/homepage/hero/Images.vue';
                             </div>
                         </div>
                         <div class="card">
-                            <div class="flex flex-row-reverse items-start gap-6 md:flex-row row">
-                                <img class="object-cover h-full rounded-md w-28 md:h-36 md:w-56"
-                                    src="@/assets/img/images/img-card-artdir.png" alt="card.png" />
+                            <div class="flex flex-row items-start gap-6 md:flex-row row">
+                                <div class="bg-red-300 images-card">
+                                    <img class="h-full" src="@/assets/img/images/img-card-artdir.png" alt="card.png" />
+                                </div>
                                 <div class="flex flex-col gap-4 content-card">
-                                    <a href="#" class="text-base font-bold text-gray-200 md:text-xl">
+                                    <a href="#" class="text-sm font-bold text-gray-200 md:text-xl">
                                         Nekat Jadi PM Director
                                     </a>
                                     <p
@@ -69,11 +74,12 @@ import Images from '../components/homepage/hero/Images.vue';
                             </div>
                         </div>
                         <div class="card">
-                            <div class="flex flex-row-reverse items-start gap-6 md:flex-row row">
-                                <img class="object-cover h-full rounded-md w-28 md:h-36 md:w-56"
-                                    src="@/assets/img/images/img-card-artdir.png" alt="card.png" />
+                            <div class="flex flex-row items-start gap-6 md:flex-row row">
+                                <div class="bg-red-300 images-card">
+                                    <img class="h-full" src="@/assets/img/images/img-card-artdir.png" alt="card.png" />
+                                </div>
                                 <div class="flex flex-col gap-4 content-card">
-                                    <a href="#" class="text-base font-bold text-gray-200 md:text-xl">
+                                    <a href="#" class="text-sm font-bold text-gray-200 md:text-xl">
                                         Nekat Jadi PM Director
                                     </a>
                                     <p
@@ -86,11 +92,12 @@ import Images from '../components/homepage/hero/Images.vue';
                             </div>
                         </div>
                         <div class="card">
-                            <div class="flex flex-row-reverse items-start gap-6 md:flex-row row">
-                                <img class="object-cover h-full rounded-md w-28 md:h-36 md:w-56"
-                                    src="@/assets/img/images/img-card-artdir.png" alt="card.png" />
+                            <div class="flex flex-row items-start gap-6 md:flex-row row">
+                                <div class="bg-red-300 images-card">
+                                    <img class="h-full" src="@/assets/img/images/img-card-artdir.png" alt="card.png" />
+                                </div>
                                 <div class="flex flex-col gap-4 content-card">
-                                    <a href="#" class="text-base font-bold text-gray-200 md:text-xl">
+                                    <a href="#" class="text-sm font-bold text-gray-200 md:text-xl">
                                         Nekat Jadi PM Director
                                     </a>
                                     <p
