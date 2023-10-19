43567d3c29ab31f8b7a426855f40fa2f7abe059e
codexyan <mdcodeid@gmail.com>
2023-10-19 08:31:46 +0700

    update codes again

--- src/components/layouts/Navbar.vue
+++ src/components/layouts/Navbar.vue
@@ -8,25 +8,25 @@ import { RouterLink } from "vue-router"
         <div
             class="container relative flex flex-wrap items-center justify-between px-4 md:mx-auto md:my-4 md:max-w-7xl md:px-24">
             <!-- Logo -->
-            <img src="@/assets/img/logo/logoweb.png" alt="logoweb.png" class="h-8">
+            <img src="@/assets/img/logo/logoweb.png" alt="logoweb.png" class="h-6 md:h-8">
 
             <!-- Navigation Link -->
             <div id="mobile-menu" class="bg-gray-900 rounded-full md:bg-gray-900 md:rounded-full">
-                <ul class="flex flex-row items-center w-full gap-5 py-3 px-7">
+                <ul class="flex flex-row items-center w-full gap-3 px-5 py-2 md:py-3 md:gap-5 md:px-7">
                     <li>
-                        <RouterLink to="/" class="block text-sm text-white rounded " aria-current="page">
+                        <RouterLink to="/" class="block text-xs text-white rounded md:text-sm " aria-current="page">
                             Home</RouterLink>
                     </li>
                     <li>
-                        <RouterLink to="/about" class="block text-sm text-gray-600">
+                        <RouterLink to="/about" class="block text-xs text-gray-600 md:text-sm">
                             About</RouterLink>
                     </li>
                     <li>
-                        <RouterLink to="/blog" class="block text-sm text-gray-600">
+                        <RouterLink to="/blog" class="block text-xs text-gray-600 md:text-sm">
                             Blog</RouterLink>
                     </li>
                     <li>
-                        <RouterLink to="/project" class="block text-sm text-gray-600">
+                        <RouterLink to="/project" class="block text-xs text-gray-600 md:text-sm">
                             Project</RouterLink>
                     </li>
                 </ul>
