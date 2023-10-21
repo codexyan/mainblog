9b1a5e00d5e2276cc8715fbaceda1990322acff7
codexyan <mdcodeid@gmail.com>
2023-10-21 10:15:25 +0700

    add project view

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,35 +1,165 @@
 <template>
     <main>
-        <!-- top project -->
-        <div class="container relative mx-auto">
-            <img src="../assets/img/images/top-card.png" alt="top-card"
-                class="w-full md:w-2/3 rounded-lg shadow-lg shadow-white-500/50" />
-            <div class="absolute bottom-0 md:w-2/3 items-center text-center transform flex justify-center content-cta">
-                <div
-                    class="content flex flex-col md:w-full mx-auto gap-3 text-left mt-5 p-5  bg-gray-500 bg-opacity-50 rounded-lg">
-                    <div class="left flex flex-col gap-2 w-11/12 md:w-5/6">
-                        <p
-                            class="category text-white py-1 md:py-2 px-3 bg-gradient-to-r from-blue-500 via-sky-400 to-purple-500 w-max rounded-full text-xs">
-                            Creative Production
-                        </p>
-                        <p class="text-white text-base md:text-xl font-semibold">
-                            Directing Company Profiles Vodjo for Avoskin as a Customer Video
-                            Profiles
-                        </p>
-                        <p class="md:inline hidden text-xs font-light text-white leading-5">
-                            Avoskin is always consistent in providing the best products from
-                            natural ingredients. No wonder now that Avoskin can become one of
-                            the best-selling skincare brands in Indonesia.
-                        </p>
+        <section id="creative-production">
+            <h1 class="text-white text-xl font-semibold mb-6">‣ Creative Production</h1>
+            <!-- top project -->
+            <div class="container mx-auto flex md:flex-row flex-col gap-6">
+                <!-- Card -->
+                <div class="bottom-0 md:w-2/3 items-center text-center transform flex justify-center content-cta rounded-lg">
+                    <div
+                        class="content flex flex-col md:w-full mx-auto gap-3 text-left mt-5 p-5 bg-gray-500 bg-opacity-50 rounded-lg">
+                        <div class="left flex flex-col gap-2 md:w-5/6">
+                            <p
+                                class="category text-white py-1 md:py-2 px-3 bg-gradient-to-r from-blue-500 via-sky-400 to-purple-500 w-max rounded-full text-xs">
+                                Creative Production
+                            </p>
+                            <p class="text-white text-base md:text-xl font-semibold">
+                                Directing Company Profiles Vodjo for Avoskin as a Customer Video
+                                Profiles
+                            </p>
+                            <p class="md:inline hidden text-xs font-light text-white leading-5">
+                                Avoskin is the best products from natural ingredients. No wonder now that Avoskin can become one of
+                                the best-selling skincare brands in Indonesia.
+                            </p>
+                        </div>
+                        <div class="right w-max flex justify-start items-center gap-2 md:gap-3">
+                            <img src="../assets/img/icons/cta-link.png" alt="cta.png"
+                                class="mx-auto bg-gray-200 md:p-2 rounded-full cursor-pointer p-1 w-6 md:w-8" />
+                            <a href="https://www.youtube.com/watch?v=Z3C6deY0iag"
+                                class="text-gray-300 py-1 md:py-2 px-4 text-xs font-medium bg-gray-500 rounded-full w-full">youtube.com/watch?v=Z3C6deY0iag</a>
+                        </div>
                     </div>
-                    <div class="right w-max flex justify-start items-center gap-2 md:gap-3">
-                        <img src="../assets/img/icons/cta-link.png" alt="cta.png"
-                            class="mx-auto bg-gray-200 md:p-2 rounded-full cursor-pointer p-1 w-6 md:w-8" />
-                        <a href="https://www.youtube.com/watch?v=Z3C6deY0iag"
-                            class="text-gray-300 py-1 md:py-2 px-4 text-xs font-medium bg-gray-500 rounded-full w-full">youtube.com/watch?v=Z3C6deY0iag</a>
+                </div>
+    
+                <div class="bottom-0 md:w-2/3 items-center text-center transform flex justify-center content-cta-second rounded-lg">
+                    <div
+                        class="content flex flex-col w-full md:w-full mx-auto gap-3 text-left mt-5 p-5 bg-gray-500 bg-opacity-50 rounded-lg">
+                        <div class="left flex flex-col gap-2 md:w-5/6">
+                            <p
+                                class="category text-white py-1 md:py-2 px-3 bg-gradient-to-r from-blue-500 via-sky-400 to-purple-500 w-max rounded-full text-xs">
+                                Creative Production
+                            </p>
+                            <p class="text-white text-base md:text-xl font-semibold">
+                                Directing Profil Dusun Lanteng 1 x MERTI DUSUN
+                            </p>
+                            <p class="md:inline hidden text-xs font-light text-white leading-5">
+                                Avoskin is always consistent in providing the best products from
+                                natural ingredients. No wonder now that Avoskin can become one of
+                                the best-selling skincare brands in Indonesia.
+                            </p>
+                        </div>
+                        <div class="right w-max flex justify-start items-center gap-2 md:gap-3">
+                            <img src="../assets/img/icons/cta-link.png" alt="cta.png"
+                                class="mx-auto bg-gray-200 md:p-2 rounded-full cursor-pointer p-1 w-6 md:w-8" />
+                            <a href="https://www.youtube.com/watch?v=WCCrA920jyI&t=324s"
+                                class="text-gray-300 py-1 md:py-2 px-4 text-xs font-medium bg-gray-500 rounded-full w-full">youtube.com/watch?v=Z3C6deY0iag</a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
+
+        <section id="developer" class="my-8">
+            <h1 class="text-white text-xl font-semibold mb-2">‣ Web Deploying</h1>
+
+            <div class="container mx-auto flex md:flex-row flex-col gap-6">
+                <!-- Card -->
+                <div class="bottom-0 md:w-2/3 items-center text-center transform flex justify-center rounded-lg">
+                    <div
+                        class="content flex flex-col md:w-full mx-auto gap-3 text-left mt-5 p-5 bg-gray-500 bg-opacity-50 rounded-lg">
+                        <div class="banner-card relative">
+                            <img src="../assets/img/images/card-web.png" alt="cardweb.png" class="rounded-md">
+                            <p
+                                class="category absolute top-0 m-3 text-white py-1 md:py-2 px-3 bg-gradient-to-r from-blue-800 via-sky-400 to-purple-500 w-max rounded-full text-xs">
+                                Front-end Deployed
+                            </p>
+                        </div>
+                        <div class="left flex flex-col gap-2 md:w-full">
+                            <p class="text-white text-base md:text-xl font-semibold">
+                                Healingoo Landing Pages
+                            </p>
+                            <p class="md:inline hidden text-xs font-light text-gray-400 leading-5">
+                                Avoskin is the best products from natural ingredients. No wonder now that Avoskin can become one of
+                                the best-selling skincare brands in Indonesia.
+                            </p>
+                        </div>
+                        <div class="right w-max flex justify-start items-center gap-2 md:gap-3">
+                            <img src="../assets/img/icons/cta-link.png" alt="cta.png"
+                                class="mx-auto bg-gray-200 md:p-2 rounded-full cursor-pointer p-1 w-6 md:w-8" />
+                            <a href="#"
+                                class="text-gray-300 py-1 md:py-2 px-4 text-xs font-medium bg-gray-500 rounded-full w-full">bit.ly/comingsoon</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="bottom-0 md:w-2/3 items-center text-center transform flex justify-center rounded-lg">
+                    <div
+                        class="content flex flex-col md:w-full mx-auto gap-3 text-left mt-5 p-5 bg-gray-500 bg-opacity-50 rounded-lg">
+                        <div class="banner-card relative">
+                            <img src="../assets/img/images/card-web.png" alt="cardweb.png" class="rounded-md">
+                            <p
+                                class="category absolute top-0 m-3 text-white py-1 md:py-2 px-3 bg-gradient-to-r from-blue-800 via-sky-400 to-purple-500 w-max rounded-full text-xs">
+                                Front-end Deployed
+                            </p>
+                        </div>
+                        <div class="left flex flex-col gap-2 md:w-full">
+                            <p class="text-white text-base md:text-xl font-semibold">
+                                Healingoo Landing Pages
+                            </p>
+                            <p class="md:inline hidden text-xs font-light text-gray-400 leading-5">
+                                Avoskin is the best products from natural ingredients. No wonder now that Avoskin can become one of
+                                the best-selling skincare brands in Indonesia.
+                            </p>
+                        </div>
+                        <div class="right w-max flex justify-start items-center gap-2 md:gap-3">
+                            <img src="../assets/img/icons/cta-link.png" alt="cta.png"
+                                class="mx-auto bg-gray-200 md:p-2 rounded-full cursor-pointer p-1 w-6 md:w-8" />
+                            <a href="#"
+                                class="text-gray-300 py-1 md:py-2 px-4 text-xs font-medium bg-gray-500 rounded-full w-full">bit.ly/comingsoon</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="bottom-0 md:w-2/3 items-center text-center transform flex justify-center rounded-lg">
+                    <div
+                        class="content flex flex-col md:w-full mx-auto gap-3 text-left mt-5 p-5 bg-gray-500 bg-opacity-50 rounded-lg">
+                        <div class="banner-card relative">
+                            <img src="../assets/img/images/card-web.png" alt="cardweb.png" class="rounded-md">
+                            <p
+                                class="category absolute top-0 m-3 text-white py-1 md:py-2 px-3 bg-gradient-to-r from-blue-800 via-sky-400 to-purple-500 w-max rounded-full text-xs">
+                                Front-end Deployed
+                            </p>
+                        </div>
+                        <div class="left flex flex-col gap-2 md:w-full">
+                            <p class="text-white text-base md:text-xl font-semibold">
+                                Healingoo Landing Pages
+                            </p>
+                            <p class="md:inline hidden text-xs font-light text-gray-400 leading-5">
+                                Avoskin is the best products from natural ingredients. No wonder now that Avoskin can become one of
+                                the best-selling skincare brands in Indonesia.
+                            </p>
+                        </div>
+                        <div class="right w-max flex justify-start items-center gap-2 md:gap-3">
+                            <img src="../assets/img/icons/cta-link.png" alt="cta.png"
+                                class="mx-auto bg-gray-200 md:p-2 rounded-full cursor-pointer p-1 w-6 md:w-8" />
+                            <a href="#"
+                                class="text-gray-300 py-1 md:py-2 px-4 text-xs font-medium bg-gray-500 rounded-full w-full">bit.ly/comingsoon</a>
+                        </div>
+                    </div>
+                </div>
+    
+            </div>
+        </section>
     </main>
 </template>
+
+<style scoped>
+.content-cta {
+    background-image: url('../assets/img/images/p-vodjo.png');
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+.content-cta-second {
+    background-image: url('../assets/img/images/p-kate.png');
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+</style>
