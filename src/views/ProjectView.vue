a9587a3e3c2445dbe88a552420d640b1c4f7653c
codexyan <mdcodeid@gmail.com>
2023-10-21 07:53:28 +0700

    add project view

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -3,22 +3,33 @@
         <!-- top project -->
         <div class="container relative mx-auto">
             <img src="../assets/img/images/top-card.png" alt="top-card"
-                class="w-full md:w-2/3 rounded-lg shadow-lg shadow-white-500/50">
+                class="w-full md:w-2/3 rounded-lg shadow-lg shadow-white-500/50" />
             <div class="absolute bottom-0 md:w-2/3 items-center text-center transform flex justify-center content-cta">
-                <div class="content flex flex-col md:w-full mx-auto gap-3 text-left mt-5 p-5 bg-gray-500 bg-opacity-50 rounded-lg">
-                    <div class="left flex flex-col gap-2 w-5/6">
-                        <p class="category text-white py-2 px-3 bg-gradient-to-r from-blue-500 via-sky-400 to-purple-500 w-max rounded-full text-xs">Creative Production</p>
-                        <p class="text-white text-base md:text-xl font-semibold">Directing Company Profiles Vodjo for
-                            Avoskin as a Customer Video Profiles</p>
-                        <p class="text-xs font-light text-white leading-5">Avoskin is always consistent in providing the best products from natural ingredients.  No wonder now that Avoskin can become one of the best-selling skincare brands in Indonesia.</p>
+                <div
+                    class="content flex flex-col md:w-full mx-auto gap-3 text-left mt-5 p-5  bg-gray-500 bg-opacity-50 rounded-lg">
+                    <div class="left flex flex-col gap-2 w-11/12 md:w-5/6">
+                        <p
+                            class="category text-white py-1 md:py-2 px-3 bg-gradient-to-r from-blue-500 via-sky-400 to-purple-500 w-max rounded-full text-xs">
+                            Creative Production
+                        </p>
+                        <p class="text-white text-base md:text-xl font-semibold">
+                            Directing Company Profiles Vodjo for Avoskin as a Customer Video
+                            Profiles
+                        </p>
+                        <p class="md:inline hidden text-xs font-light text-white leading-5">
+                            Avoskin is always consistent in providing the best products from
+                            natural ingredients. No wonder now that Avoskin can become one of
+                            the best-selling skincare brands in Indonesia.
+                        </p>
                     </div>
-                    <div class="right w-max flex justify-start items-center gap-3 ">
+                    <div class="right w-max flex justify-start items-center gap-2 md:gap-3">
                         <img src="../assets/img/icons/cta-link.png" alt="cta.png"
-                            class="mx-auto bg-gray-200 p-2 rounded-full cursor-pointer w-8">
-                        <a href="https://www.youtube.com/watch?v=Z3C6deY0iag" class="text-gray-300 py-2 px-4 text-xs font-medium bg-gray-500 rounded-full w-full">youtube.com/watch?v=Z3C6deY0iag</a>
+                            class="mx-auto bg-gray-200 md:p-2 rounded-full cursor-pointer p-1 w-6 md:w-8" />
+                        <a href="https://www.youtube.com/watch?v=Z3C6deY0iag"
+                            class="text-gray-300 py-1 md:py-2 px-4 text-xs font-medium bg-gray-500 rounded-full w-full">youtube.com/watch?v=Z3C6deY0iag</a>
                     </div>
                 </div>
             </div>
-    </div>
-
-</main></template>
+        </div>
+    </main>
+</template>
