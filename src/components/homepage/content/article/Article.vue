9e8e4b8be90cf0261502a8f7fa6363a0387a10bd
codexyan <mdcodeid@gmail.com>
2023-10-21 11:54:57 +0700

    add project view

--- src/components/homepage/content/article/Article.vue
+++ src/components/homepage/content/article/Article.vue
@@ -6,14 +6,14 @@
                 <img class="object-cover h-full rounded-md w-28 md:h-36 md:w-44" src="@/assets/img/images/img-card-erp.png"
                     alt="card.png" />
                 <div class="flex flex-col gap-2 content-card">
-                    <a href="http://localhost:5173/article-blog" class="text-base font-bold text-gray-200 md:text-xl">
+                    <a href="https://iyndev.vercel.app/article-blog" class="text-base font-bold text-gray-200 md:text-xl">
                         Kenalan sama Ekosistem ERP
                     </a>
                     <p class="overflow-hidden text-xs leading-4 text-gray-500 md:leading-6 md:tracking-wider md:text-sm">
                         Jelajahi Kreativitas Tanpa Batas: Pahami Gestalt Principle
                         dalam Satu Praktek!
                     </p>
-                    <a href="http://localhost:5173/article-blog" class="text-sm font-semibold text-indigo-500">Read More »</a>
+                    <a href="https://iyndev.vercel.app/article-blog" class="text-sm font-semibold text-indigo-500">Read More »</a>
                 </div>
             </div>
         </div>
@@ -25,14 +25,14 @@
                 <img class="object-cover h-full rounded-md w-28 md:h-36 md:w-44"
                     src="@/assets/img/images/img-card-artdir.png" alt="card.png" />
                 <div class="flex flex-col gap-2 content-card">
-                    <a href="http://localhost:5173/article-blog" class="text-base font-bold text-gray-200 md:text-xl">
+                    <a href="https://iyndev.vercel.app/article-blog" class="text-base font-bold text-gray-200 md:text-xl">
                         Nekat Jadi PM Director
                     </a>
                     <p class="overflow-hidden text-xs leading-4 text-gray-500 md:leading-6 md:tracking-wider md:text-sm">
                         Jelajahi Kreativitas Tanpa Batas: Pahami Gestalt Principle
                         dalam Satu Praktek!
                     </p>
-                    <a href="http://localhost:5173/article-blog" class="text-sm font-semibold text-indigo-500">Read More »</a>
+                    <a href="https://iyndev.vercel.app/article-blog" class="text-sm font-semibold text-indigo-500">Read More »</a>
                 </div>
             </div>
         </div>
@@ -44,14 +44,14 @@
                 <img class="object-cover h-full rounded-md w-28 md:h-36 md:w-44" src="@/assets/img/images/img-card.png"
                     alt="card.png" />
                 <div class="flex flex-col gap-2 content-card">
-                    <a href="http://localhost:5173/article-blog" class="text-base font-bold text-gray-200 md:text-xl">
+                    <a href="https://iyndev.vercel.app/article-blog" class="text-base font-bold text-gray-200 md:text-xl">
                         Pahami UX Gestalt Principle dalam Sekejap!
                     </a>
                     <p class="overflow-hidden text-xs leading-4 text-gray-500 md:leading-6 md:tracking-wider md:text-sm">
                         Jelajahi Kreativitas Tanpa Batas: Pahami Gestalt Principle
                         dalam Satu Praktek!
                     </p>
-                    <a href="http://localhost:5173/article-blog" class="text-sm font-semibold text-indigo-500">Read More »</a>
+                    <a href="https://iyndev.vercel.app/article-blog" class="text-sm font-semibold text-indigo-500">Read More »</a>
                 </div>
             </div>
         </div>
